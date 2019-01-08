83427be9138e7acc1a9e806f4ecd579a455bc300
Joshua Saucedo <joshuansu0897@gmail.com>
2019-01-08 11:49:22 -0700

    install and config

--- android/app/src/main/java/com/mapboxtest/MainApplication.java
+++ android/app/src/main/java/com/mapboxtest/MainApplication.java
@@ -3,11 +3,11 @@ package com.mapboxtest;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.mapbox.rctmgl.RCTMGLPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RCTMGLPackage()
+            new RCTMGLPackage()
       );
     }
 
