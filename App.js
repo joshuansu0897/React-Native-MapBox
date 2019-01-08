813aa30087cb96a43908063fd00569e964aaf9b4
Joshua Saucedo <joshuansu0897@gmail.com>
2019-01-08 12:15:56 -0700

    locaction

--- App.js
+++ App.js
@@ -7,14 +7,32 @@ import MAPBOX_TOKEN from './tokenMapBox'
 Mapbox.setAccessToken(MAPBOX_TOKEN);
 
 class App extends Component {
+
+  renderAnnotations() {
+    return (
+      <Mapbox.PointAnnotation
+        key='pointAnnotation'
+        id='pointAnnotation'
+        coordinate={[-110.968367, 29.091201]}>
+
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <Mapbox.Callout title='Look! An annotation!' />
+      </Mapbox.PointAnnotation>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Mapbox.MapView
-          styleURL={Mapbox.StyleURL.Street}
+          styleURL={Mapbox.StyleURL.Dark}
           zoomLevel={15}
-          centerCoordinate={[11.256, 43.770]}
-          style={styles.container}>
+          centerCoordinate={[-110.968367, 29.091201]}
+          style={styles.container}
+          showUserLocation={true}>
+          {this.renderAnnotations()}
         </Mapbox.MapView>
       </View>
     );
@@ -24,6 +42,21 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'orange',
+    transform: [{ scale: 0.6 }],
   },
 });
 
